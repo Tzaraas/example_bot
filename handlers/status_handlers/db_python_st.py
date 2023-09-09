0ff12e3be1fb05659890a98e01af7435fee24ceb
Fedor Dzyatko <t9odop@gmail.com>
2023-09-09 17:40:30 +0300

    try add search keyword

--- handlers/status_handlers/db_python_st.py
+++ handlers/status_handlers/db_python_st.py
@@ -16,7 +16,7 @@ def db_python_call(call: CallbackQuery):
         case "collections_types":
             bot.send_photo(call.from_user.id, open(r'utils\picture\collections_types.jpg', 'rb'))
         case "str":
-            get_data(call, "str")
+            get_query(call, "str")
             bot.send_message(call.from_user.id, "https://pythonworld.ru/tipy-dannyx-v-python/stroki-funkcii-i-metody-strok.html")
         case "list":
             get_data(call, "list")
@@ -28,13 +28,16 @@ def db_python_call(call: CallbackQuery):
             bot.edit_message_text("Выберите интересующий раздел:", call.from_user.id, call.message.id, reply_markup=db_python.menu_button())
 
 
+def get_query(call: CallbackQuery, collection: str):
+    bot.send_message(call.from_user.id, "Введите ключевое слово или оставьте поле пустым и нажмите Enter:")
+    bot.register_next_step_handler_by_chat_id(call.message.id, get_data(call, collection, call.data))
+
+def get_data(call: CallbackQuery, collection: str, query: str):
+    for method in DB_Method.select(DB_Method.met_name, DB_Method.met_desc).join(DB_Coll).where(DB_Coll.coll_name == collection and DB_Method.met_desc % query):
+        bot.send_message(call.from_user.id, method, parse_mode="html")
+
+
 @bot.message_handler(state=Status.db_python)
 def db_python_text(message: Message):
     bot.set_state(message.from_user.id, Status.main)
     bot.reply_to(message, "Прости, я прослушал. Повтори, пожалуйста.")
-
-
-def get_data(call, query):
-    for method in DB_Method.select(DB_Method.met_name, DB_Method.met_desc).join(DB_Coll).where(DB_Coll.coll_name == query):
-        bot.send_message(call.from_user.id, method, parse_mode="html")
-    
